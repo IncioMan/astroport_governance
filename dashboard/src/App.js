bf58fc750f193162118d6b69b223ba4ba5d85f6b
Alex Incerti <alex.incerti@outlook.com>
2022-05-01 10:07:51 +0200

    dataaaa

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -13,6 +13,7 @@ import { Bar, Doughnut } from 'react-chartjs-2';
 import { faker } from '@faker-js/faker';
 import { ArcElement } from "chart.js";
 import 'chart.js/auto';
+import { useEffect, useState } from 'react';
 const axios = require('axios').default;
 
 
@@ -28,69 +29,93 @@ ChartJS.defaults.color = "#fff";
 ChartJS.defaults.backgroundColor = "#fff";
 ChartJS.defaults.borderColor = "#fff";
 
-axios.get("https://raw.githubusercontent.com/IncioMan/astroport_governance/master/data/proposal_recap")
+
+
+
+export default function App() {
+  const [proposalData, setProposalData] = useState([])
+  const [proposalChartData, setProposalChartData] = useState({options:null, data:null})
+  
+  useEffect(()=>{
+    axios.get("https://raw.githubusercontent.com/IncioMan/astroport_governance/master/data/proposal_recap")
         .then(function (response) {
-            console.log(response.data)
+          setProposalData(response.data)
+          console.log("habemus data")
         })
         .catch(function (error) {
             console.log(error);
         })
+  },[])
 
+  useEffect(()=>{
+    if(proposalData.length == 0){
+      return
+    }
+    const labels = [1,2,3,4,5,6,7];
+    const options = {
+      plugins: {
+        title: {
+          display: false,
+          text: 'Chart.js Bar Chart - Stacked',
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+          grid:{
+            display: false
+          }
+        },
+        y: {
+          stacked: true
+        },
+      },
+    };
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'Against',
+          data: labels.map((i) => proposalData.filter((p)=>parseInt(p.proposal_id)==i).map((p)=>p.voting_power_against)),
+          backgroundColor: 'rgb(255, 99, 132)',
+        },
+        {
+          label: 'For',
+          data: labels.map((i) => proposalData.filter((p)=>parseInt(p.proposal_id)==i).map((p)=>p.voting_power_for)),
+          backgroundColor: 'rgb(75, 192, 192)',
+        },
+        {
+          label: 'Abstain',
+          data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+          backgroundColor: 'rgb(223, 223, 222)',
+        },
+      ],
+    }
+    setProposalChartData({
+      options: options,
+      data: data
+    })
+    console.log(proposalChartData, proposalData)
+  },[proposalData])
+  
 
-export const options = {
-  plugins: {
-    title: {
-      display: false,
-      text: 'Chart.js Bar Chart - Stacked',
-    },
-  },
-  responsive: true,
-  scales: {
-    x: {
-      stacked: true,
-    },
-    y: {
-      stacked: true,
-    },
-  },
-};
-
-const labels = ['#1','#2','#3','#4'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Against',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgb(255, 99, 132)',
-    },
-    {
-      label: 'For',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgb(75, 192, 192)',
-    },
-    {
-      label: 'Abstain',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
-      backgroundColor: 'rgb(223, 223, 222)',
-    },
-  ],
-};
-
-export default function App() {
   return (
     <div className='App'>
         <div className='App-header'>
           <div className='charts-container'>
             <div className='chart-container'>
-              <Bar options={options} data={data} />
+              { (proposalChartData.data)&&
+                <Bar options={proposalChartData.options} data={proposalChartData.data}/>
+              }
             </div>
             <div className='chart-container'>
-              <Bar options={options} data={data} />
+              { (proposalChartData.data)&&
+                <Bar options={proposalChartData.options} data={proposalChartData.data}/>
+              }
             </div>
         </div>
       </div>
-        </div>
+    </div>
   );
 }
